fa82a8f9c41fefa61d252ba5a48d9bc49548e67f
Jorge jr <jorgesilvajunior10@gmail.com>
2023-03-30 14:57:41 -0300

    commit v2

--- ex4.js
+++ ex4.js
@@ -1,6 +1,4 @@
-var fs = require("fs");
-let data = fs.readFileSync("dados4.json", "utf8");
-let dados = JSON.parse(data)
+let dados = [{"estado": "SP", "valor": 67836.43},{"estado": "RJ", "valor": 36678.66}, {"estado": "MG",  "valor": 29229.88},{"estado": "ES", "valor": 27165.48 },{"estado": "Outros", "valor": 19849.53}];
 
 var state = "MG" 
 
